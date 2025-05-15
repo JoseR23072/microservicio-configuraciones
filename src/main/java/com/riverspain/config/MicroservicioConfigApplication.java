38d4cba4ba96734f90377045f9a080e50c29be07
JoseR <josequispeogosi12927@gmail.com>
2025-05-15 18:14:00 +0200

    prueba desligue 13 eliminando actuator

--- src/main/java/com/riverspain/config/MicroservicioConfigApplication.java
+++ src/main/java/com/riverspain/config/MicroservicioConfigApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
-@SpringBootApplication(exclude = {
-    org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration.class
-})
+@SpringBootApplication
 public class MicroservicioConfigApplication {
 
 	public static void main(String[] args) {
